9e6e68e88814bf345afaabd19373fabda6b14189
Kaushik-Iyer-skima <kaushik.iyer@skima.ai>
2024-05-23 18:58:37 +0530

    change auth flow

--- app/routers/temp.py
+++ app/routers/temp.py
@@ -1,11 +1,19 @@
 import os
 from pydantic import BaseModel
 import geocoder
+from typing import Optional
+import jwt
+from jose import JWTError
 from dotenv import load_dotenv
-from fastapi import HTTPException
+from fastapi import HTTPException , Header
 from starlette.requests import Request
+from fastapi.security import OAuth2PasswordBearer
 load_dotenv()
 maps_api_key=os.getenv('MAPS_API_KEY')
+secret_key=os.getenv('SECRET_KEY')
+algorithm="HS256"
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 
 def fix_object_id(doc):
     if "_id" in doc:
@@ -18,9 +26,25 @@ def get_lat_long():
 
 def get_current_user(request: Request):
     user = request.session.get('user')
-    if user is None:
-        raise HTTPException(status_code=401, detail='Not authenticated')
     return user
+
+def verify_jwt(authorization: Optional[str] = Header(None)):
+    if not authorization:
+        raise HTTPException(status_code=403, detail='Authorization header is missing')
+    
+    scheme, _, token = authorization.partition(' ')
+    
+    if scheme.lower() != 'bearer':
+        raise HTTPException(status_code=401, detail='Invalid authorization scheme')
+    
+    if not token:
+        raise HTTPException(status_code=403, detail='Token is missing')
+    
+    try:
+        payload = jwt.decode(token, secret_key, algorithms=[algorithm])
+        return payload
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
 
 class Player(BaseModel):
     name: str
@@ -33,4 +57,3 @@ class Player(BaseModel):
             "goals": self.goals,
             "assists": self.assists
         }
-
